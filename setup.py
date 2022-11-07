c861e31755d6819f912eaafe3681e3e6a6d38c73
FYLSunghwan <sunghwan@scatterlab.co.kr>
2022-11-08 02:02:52 +0900

    chore: py39 template

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="beam_anonymizer",
+    version="0.0.1",
+    description="asdf",
+    install_requires=[],
+    url="https://github.com/FYLSunghwan/beam-anonymizer.git",
+    author="FYLSunghwan",
+    author_email="sunghwan519@hotmail.com",
+    packages=find_packages(exclude=["tests"]),
+)
