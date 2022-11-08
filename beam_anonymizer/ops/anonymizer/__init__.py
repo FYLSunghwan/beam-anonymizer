6a7325e41385bf4b90e490df207992b26b301e96
FYLSunghwan <sunghwan@scatterlab.co.kr>
2022-11-08 21:02:34 +0900

    feat: partial suppression 구현

--- beam_anonymizer/ops/anonymizer/__init__.py
+++ beam_anonymizer/ops/anonymizer/__init__.py
@@ -2,5 +2,6 @@ from .grouping import Grouping
 from .masking import Masking
 from .micro_aggregation import MicroAggregation
 from .rounding import Rounding
+from .partial_suppression import PartialSuppression
 
-__all__ = ["Grouping", "Masking", "Rounding", "MicroAggregation"]
+__all__ = ["Grouping", "Masking", "Rounding", "MicroAggregation", "PartialSuppression"]
