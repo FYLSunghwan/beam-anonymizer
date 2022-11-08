e275668feb09f1a4223bda9fc48d9bf1b6042936
FYLSunghwan <sunghwan@scatterlab.co.kr>
2022-11-08 20:53:49 +0900

    feat: micro_aggregation 구현

--- beam_anonymizer/ops/anonymizer/__init__.py
+++ beam_anonymizer/ops/anonymizer/__init__.py
@@ -1,5 +1,6 @@
 from .grouping import Grouping
 from .masking import Masking
+from .micro_aggregation import MicroAggregation
 from .rounding import Rounding
 
-__all__ = ["Grouping", "Masking", "Rounding"]
+__all__ = ["Grouping", "Masking", "Rounding", "MicroAggregation"]
